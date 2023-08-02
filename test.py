0730364e4cb8760f957cabc58ecb89235fc93df0
Lou Lopez <lolopez@student.42lyon.fr>
2023-08-02 15:02:11 +0200

    test meilleure validation

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
 from mne.channels import make_standard_montage
 from mne.decoding import CSP
-from sklearn.pipeline import Pipeline
+from sklearn import svm
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import train_test_split, ShuffleSplit, cross_val_score
+from sklearn.model_selection import train_test_split, ShuffleSplit,GridSearchCV, cross_val_score, StratifiedKFold
 import numpy as np
 
 
@@ -53,10 +54,19 @@ epochs_data_train = epochs_train.get_data()
 # Diviser les données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(epochs_data_train, Y, test_size=0.2, random_state=42)
 # Créer la pipeline avec CSP comme prétraitement et LDA comme modèle de classification
+csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
 pipeline = Pipeline([
-    ('CSP', CSP(n_components=4, reg=None, log=True, norm_trace=False)),  # Prétraitement CSP
+    ('CSP', csp),  # Prétraitement CSP
     ('LDA', LinearDiscriminantAnalysis())  # Modèle de classification LDA
 ])
+clf_svm_pip = make_pipeline(csp, svm.SVC(random_state=42))
+parameters = {'svc__kernel':['linear', 'rbf', 'sigmoid'], 'svc__C':[0.1, 1, 10]}
+gs_cv_svm = GridSearchCV(clf_svm_pip, parameters, scoring='accuracy', cv=StratifiedKFold(n_splits=5), return_train_score=True)
+gs_cv_svm.fit(X_train, y_train)
+print('Best Parameters: {}'.format(gs_cv_svm.best_params_))
+print('Best Score: {}'.format(gs_cv_svm.best_score_))
+prediction_svm = gs_cv_svm.predict(X_test)
+print("SVM Précision sur l'ensemble de test:", gs_cv_svm.score(X_test, y_test))
 
 # Entraîner le modèle avec la validation croisée
 scores = cross_val_score(pipeline, X_train, y_train, cv=5)  # 5-fold cross-validation
